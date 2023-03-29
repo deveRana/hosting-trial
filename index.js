ece62fb4395868761bd55d5c77edcbeedab13cf1
mohit <mohitpardeshi729@gmail.com>
2023-03-29 23:30:52 +0530

    intial commit with a basic template

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+//middlewares
+app.use(express.json());
+
+//get route on root
+app.get('/',(req,res)=>{
+    res.status(200).json({msg:'this is the get route and its working'});    
+})
+
+//post route
+app.post('/',(req,res)=>{
+    res.status(200).json({msg:'this is the post route and its working'});        
+})
+
+//listen on this port
+app.listen(PORT , ()=>{
+    console.log(`server is running on port ${3000}`)
+})
